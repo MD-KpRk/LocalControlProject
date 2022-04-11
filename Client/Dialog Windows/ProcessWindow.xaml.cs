33b7d694d80548333fc957e30f01e5f0a4acb5b4
MD-KpRk <griferad@gmail.com>
2022-04-11 17:59:55 +0300

    Добавил окно для управления процессами

---
+++ Client/Dialog Windows/ProcessWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using System.Drawing;         //For Icon
+using System.Reflection;      //For Assembly
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Client.Dialog_Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для ProcessWindow.xaml
+    /// </summary>
+    public partial class ProcessWindow : Window
+    {
+        public ProcessWindow()
+        {
+            //var icon = Process.GetProcessById(1234).GetIcon()
+            InitializeComponent();
+        }
+    }
+
+
+
+    
+}
